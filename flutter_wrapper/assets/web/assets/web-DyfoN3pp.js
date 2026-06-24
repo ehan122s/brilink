4e64a30df93b4ae058133103a228a4d8acdcf1d0
Reyhan <hannn4you@gmail.com>
2026-06-25 00:20:57 +0700

    Fix GitHub Pages routing for BukuLink

--- flutter_wrapper/assets/web/assets/web-DyfoN3pp.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-yZBw8AtF.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,25 @@ import "./styles/app.css";
 
 initializeNativeApp();
 
+function getRouterBasename() {
+  if (typeof window === "undefined") {
+    return undefined;
+  }
+
+  if (window.location.hostname.endsWith("github.io")) {
+    const pathSegments = window.location.pathname.split("/").filter(Boolean);
+
+    if (pathSegments[0] === "brilink") {
+      return "/brilink";
+    }
+  }
+
+  return undefined;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={getRouterBasename()}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
